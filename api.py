1804e2f138e8a030ac0e4028ac23bb5c907166cd
Skandamurthy <skandasneha@gmail.com>
2018-08-24 16:58:38 +0530

    Add to cart issue not solved.

--- api.py
+++ api.py
@@ -1,10 +1,11 @@
 from flask import Flask, request ,render_template , redirect , url_for,session
 from models.user_model import user_signup,search_user_by_username,check_user
 from models.seller_model import new_product,seller_products
-from models.buyer_model import buyer_products	
+from models.buyer_model import buyer_products,cart_details,update_cart_details
 
 app = Flask(__name__)
 app.secret_key = 'skanda'
+
 @app.route("/")
 def index():
 	if ("user_id" in session.keys()):
@@ -36,16 +37,18 @@ def logout():
 @app.route('/signup', methods = ['POST'])
 def signup():
 	user_info = {}
+	user_info["name"] = request.form["name"]
 	user_info["username"] = request.form["username"]
 	user_info["password"] = request.form["password"]
 	user_info["email"] = request.form["email"]
 	user_info["account_type"] = request.form["account_type"]
-	user_info["cart_details"] =[]
+	if user_info["account_type"] =="buyer":
+		user_info["cart_details"] =[]
 	if check_user(user_info["username"]) is None:
 		results = user_signup(user_info)
 		if (results is True):
 			session['user_id'] = str(user_info['_id'])
-			return ("Successful")
+			return render_template(url_for('index'))
 	else:
 		return render_template('error.html',message = 'signup failed')
 
@@ -79,9 +82,16 @@ def add_to_cart_done():
 
 @app.route('/add_to_cart', methods=['POST'])
 def add_to_cart():
+	#import pdb;pdb.set_trace()
 	product_id =request.form["product_id"]
-	print(product_id)
+	res=update_cart_details(session["user_id"],product_id)
+	print(res)	
 	return redirect(url_for("products_function"))
+
+@app.route('/cart_page')
+def cart_page():
+	cart_list = cart_details(session["user_id"])
+	return render_template("cart_page.html",cart_list=cart_list)
 	
 
 if (__name__=="__main__"):
--- models/buyer_model.py
+++ models/buyer_model.py
@@ -8,5 +8,18 @@ def buyer_products():
 	for post in result1:
 		ans.append(post)
 	return ans
+	
+def cart_details(user_id):
+	cart_list =[]
+	#print(product_id)
+	filter_query1 = {"_id":user_id}
+	results = db["users"].find_one(filter_query1)
+	return results
 
+def update_cart_details(user_id,product_id):
+	print(product_id)
+	print(user_id)
+	return db["users"].update({"_id":ObjectId(user_id)},{"$addToSet":{"cart_details":[product_id]}})
+
+	
 #print(buyer_products()) 	
