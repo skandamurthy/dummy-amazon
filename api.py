2bfbb01db7863580bfb08faac98b5192716ed83b
Skandamurthy <skandasneha@gmail.com>
2018-08-30 00:33:47 +0530

    Add to cart feature with number of products added

--- api.py
+++ api.py
@@ -9,20 +9,20 @@ app.secret_key = 'skanda'
 @app.route("/")
 def index():
 	if ("user_id" in session.keys()):
-		return render_template('welcome.html', login="True")
+		return render_template('welcome.html', login = "True")
 	else:
-		return render_template('welcome.html' , login="False")
+		return render_template('welcome.html' , login = "False")
 
 
-@app.route("/login",methods=['POST'])
+@app.route("/login",methods = ['POST'])
 def login():
 	inbound_username = request.form["username"]
 	existing_user = search_user_by_username(inbound_username)
 	if(existing_user is None):
-		return render_template('error.html', message='Not a user,Sign-up to login')
-	elif(request.form['password']==existing_user['password']):
-		#print(existing_user)		
-		session['user_id'] =str(existing_user['_id'])
+		return render_template('error.html', message = 'Not a user,Sign-up to login')
+	elif(request.form['password'] == existing_user['password']):
+		#print(existing_user)
+		session['user_id'] = str(existing_user['_id'])
 		session['account_type'] = str(existing_user['account_type'])
 		session['username'] = existing_user['username']
 		return redirect(url_for('index'))
@@ -31,7 +31,7 @@ def login():
 
 @app.route('/logout')
 def logout():
-	session.pop('user_id' ,None)
+	session.pop('user_id' , None)
 	return redirect(url_for('index'))
 
 @app.route('/signup', methods = ['POST'])
@@ -42,8 +42,8 @@ def signup():
 	user_info["password"] = request.form["password"]
 	user_info["email"] = request.form["email"]
 	user_info["account_type"] = request.form["account_type"]
-	if user_info["account_type"] =="buyer":
-		user_info["cart_details"] =[]
+	if user_info["account_type"] == "buyer":
+		user_info["cart_details"] = []
 	if check_user(user_info["username"]) is None:
 		results = user_signup(user_info)
 		if (results is True):
@@ -55,7 +55,7 @@ def signup():
 
 @app.route("/all_products")
 def products_function():
-	if session["account_type"] =="seller":
+	if session["account_type"] == "seller":
 		result = seller_products(session["user_id"])
 	else:
 		result = buyer_products()
@@ -68,27 +68,28 @@ def func():
 @app.route('/product_details', methods = ['POST'])
 def add_product_page():
 	product_info = {}
-	product_info["name"] =request.form["name"]
+	product_info["name"] = request.form["name"]
 	product_info["price"] = request.form["price"]
 	product_info["description"] = request.form["description"]
-	product_info["user_id"] =session["user_id"]
-	product_info["username"] =session["username"]	
+	product_info["user_id"] = session["user_id"]
+	product_info["username"] = session["username"]	
 	new_product(product_info)
-	return redirect(url_for('products_function'))
+	return redirect(url_for("products_function"))			#
 
-
-@app.route('/add_to_cart', methods=['POST'])
+@app.route('/add_to_cart', methods = ['POST'])
 def add_to_cart():
 	#import pdb;pdb.set_trace()
-	product_id =request.form["product_id"]
-	update_cart_details(session["user_id"],product_id)
+	product_id = request.form["product_id"]
+	quantity = request.form["quantity"]
+	update_cart_details(session["user_id"],product_id,quantity)
 	#print(res)
-	return redirect(url_for("products_function"))
+	return redirect(url_for("cart_page"))
 
 @app.route('/cart_page')
 def cart_page():
-	cart_list = cart_details(session["user_id"])
-	return render_template("cart_page.html",cart_list=cart_list)
+	cart_dict= cart_details(session["user_id"])
+
+	return render_template("cart_page.html",cart_dict = cart_dict)
 
 if (__name__=="__main__"):
-	app.run(debug=True)
+	app.run(debug = True)
--- models/buyer_model.py
+++ models/buyer_model.py
@@ -12,19 +12,30 @@ def buyer_products():
 def cart_details(user_id):
 	#print(product_id)
 	results = []
-	filter_query1 = {"_id":ObjectId(user_id)}
+	quantity_intermediate= {}
+	filter_query1 = {"_id" : ObjectId(user_id)}
 	result = db["users"].find_one(filter_query1)
-	cart_list=result["cart_details"]
-	for item in cart_list:
-		filter_query2 = {"_id":ObjectId(item)}
-		results.append(db["products"].find_one(filter_query2))
+	cart_list = result["cart"]
+	#zip both quantity and product_id,create a different dictionary and merge them using mapping
+	for item,quantity in zip(cart_list.keys(),cart_list.values()): 
+		filter_query2 = {"_id" : ObjectId(item)}
+		result =db["products"].find_one(filter_query2)
+		quantity_intermediate["quantity"] =quantity
+		results.append({**result,**quantity_intermediate})
 	return results
 
-def update_cart_details(user_id,product_id):
+def update_cart_details(user_id,product_id,quantity):
 #return a message	
 	#print(product_id)
 	#print(user_id)
-	db["users"].update({"_id":ObjectId(user_id)},{"$addToSet":{"cart_details":{"$each":[product_id]}}})
-
-	
-#print(buyer_products()) 	
+	#print(quantity)	
+	#db["users"].update({"_id":ObjectId(user_id)},{"$addToSet":{"cart_details":{"$each":[{"product_id":product_id,"quantity":quantity}]}}})
+	quantity = int(quantity)
+	user_info = db["users"].find_one({"_id":ObjectId(user_id)})
+	cart_dict = user_info["cart"]
+	if product_id not in cart_dict.keys():
+		cart_dict[product_id] = quantity
+	else:
+		cart_dict[product_id] += quantity
+	db["users"].update({"_id" : ObjectId(user_id)},{"$set" : {"cart" : cart_dict}})
+#print(buyer_products())
